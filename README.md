e9b900bb195b12165352142240aadd4d9f755843
robashton <robashton@codeofrob.com>
2023-12-02 07:08:01 +0000

    initial commit of a skeleton helix

---
+++ README.md
@@ -0,0 +1,8 @@
+# My Helix setup
+
+
+This is likely of no interest to anybody, and simply exists as its own repo so I can get my own helix from anywhere in the world with
+
+```
+nix run github:robashton/flake-hx
+```
---
+++ flake.nix
@@ -0,0 +1,64 @@
+{
+  description = "Rob's helix";
+
+  inputs = {
+    nixpkgs.url = github:nixos/nixpkgs/nixos-unstable;
+    flake-utils.url = github:numtide/flake-utils;
+    hx.url = "github:helix-editor/helix";
+    hx.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = inputs@{ nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        helix = f: p: {
+          helix = inputs.hx.packages.${system}.helix;
+        };
+
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [helix];
+        };
+
+        cfg = pkgs.runCommand "helix-config" {} ''
+          mkdir -p $out
+          cp ${./.}/files/config.toml $out/config.toml
+        '';
+
+        hx = pkgs.writeShellScriptBin "hx" ''
+          ${pkgs.helix}/bin/hx --config ${cfg}/config.toml $@
+        '';
+      in
+      rec {
+
+        # leaving this here in case I want to override anything else..
+        robs-helix = hx;
+
+        overlays.default = f: p: {
+          inherit (pkgs);
+        };
+
+        nixosModules.hm = {
+          imports = [
+            { nixpkgs.overlays = [ overlays.default ]; }
+          ];
+        };
+
+        packages = {
+          default = robs-helix;
+        };
+
+        apps = rec {
+          helix = {
+            type = "app";
+            program = "${packages.default}/bin/hx";
+          };
+          default = helix;
+        };
+
+        devShells.default = pkgs.mkShell { 
+          buildInputs = [ robs-helix ];
+        };
+      }
+    );
+}
