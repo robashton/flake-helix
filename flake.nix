94192f80a5c375426cb8f09aae6a4fad4743776e
Rob Ashton <robashton@codeofrob.com>
2023-12-06 11:47:32 +0000

    more config

--- flake.nix
+++ flake.nix
@@ -4,34 +4,29 @@
   inputs = {
     nixpkgs.url = github:nixos/nixpkgs/nixos-unstable;
     flake-utils.url = github:numtide/flake-utils;
-    hx.url = "github:helix-editor/helix";
-    hx.inputs.nixpkgs.follows = "nixpkgs";
+#    hx.url = "github:helix-editor/helix";
+#    hx.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = inputs@{ nixpkgs, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        helix = f: p: {
-          helix = inputs.hx.packages.${system}.helix;
-        };
-
         pkgs = import nixpkgs {
           inherit system;
-          overlays = [helix];
         };
 
         cfg = pkgs.runCommand "helix-config" {} ''
-          mkdir -p $out
-          cp ${./.}/files/config.toml $out/config.toml
-        '';
-
-        _languages = pkgs.runCommand "helix-languages" {} ''
-          mkdir -p $out/config/
-          cp ${./.}/files/languages.toml $out/config/languages.toml
+          mkdir -p $out/helix/config
+          cp ${./.}/files/config.toml $out/helix/config.toml
+          cp ${./.}/files/languages.toml $out/helix/languages.toml
         '';
 
         hx = pkgs.writeShellScriptBin "hx" ''
-          ${pkgs.helix}/bin/hx --config ${cfg}/config.toml $@
+          # I would prefer not to use this env var
+          # but https://github.com/helix-editor/helix/discussions/8160
+          # helix people seem people opinionated and I can't be fucked going in
+          # to have a chat about that again
+          XDG_CONFIG_HOME=${cfg} ${pkgs.helix}/bin/hx $@
         '';
       in
       rec {
